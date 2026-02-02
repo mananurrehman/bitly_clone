624026f14ef17a3050542370509fa50005ece0a7
mananurrehman <mananurrehman@gmail.com>
2026-02-02 17:07:29 +0500

    login, signup, logout, email and password routes defined and built

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 import logging
+from flask_login import LoginManager
+from app.auth import auth
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+# login manager defined
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +19,12 @@ def create_app():
 
     try:
         db.init_app(app)
+        # initializing login manager for login sessions 
+        login_manager.init_app(app)
+
         migrate.init_app(app, db)
+        app.register_blueprint(auth)
+        
     except Exception as e:
         logging.error(f"Failed to initialize database: {e}")
 
@@ -26,3 +37,10 @@ def create_app():
         logging.error(f"Module import filed: {e}")
     
     return app
+
+# store only user-id 
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
---
+++ app/auth.py
@@ -0,0 +1,71 @@
+from flask import Blueprint, render_template, request, redirect, flash, url_for
+from app.models import User 
+from app import db
+from flask_login import login_user, logout_user, login_required
+import logging
+
+auth = Blueprint('auth', __name__)
+
+# login route
+
+@auth.route('/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email or not password:
+            flash("All fields are required!", 'error')
+            return redirect(url_for('auth.login'))
+
+# user authentication    
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        
+        else:
+            flash("Invalid email or password", "eror")
+            return redirect(url_for('auth.login'))
+        
+    return render_template('login.html')
+
+# Sign up route
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email or not password:
+            flash('All fields are required', 'error')
+            return(redirect(url_for('auth.signup')))
+        
+        if User.query.filter_by(email=email).first():
+            flash('Email already registered', 'error')
+            return(redirect(url_for('auth.signup')))
+        
+        new_user = User(email=email)
+        new_user.set_password(password)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Signup successful! Please login.', 'success')
+
+        except Exception as e:
+            logging.error(f"Failed to initialize database: {e}")
+    
+    return render_template('signup.html')
+
+
+# Logout Rotue
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logout Successfully', 'success')
+    return redirect(url_for('auth.login'))
